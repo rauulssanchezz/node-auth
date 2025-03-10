72f53c3b645a88c1847ad2cb6c5e4d0edb0ad864
Raúl Sánchez <132095151+rauulssanchezz@users.noreply.github.com>
2025-03-10 17:16:21 +0100

    FEAT: AuthController

---
+++ src/presentation/auth/controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express"
+
+
+
+export class AuthController {
+    constructor() {}
+
+    registerUser = (req: Request, res: Response) => {
+        res.json('registerUser controller')
+    }
+
+    loginUser = (req: Request, res: Response) => {
+        res.json('loginUser controller')
+    }
+}
--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express";
+import { AuthController } from "./controller";
 
 export class AuthRoutes {
 
     static get routes(): Router {
 
         const router = Router()
+        const controller = new AuthController()
 
-        router.post('/login', (req, res) => {
-            res.json('Login')
-        })
-        router.post('/register', (req, res) => {
-            res.json('Register')
-        })
+        router.post('/login', controller.loginUser)
+        
+        router.post('/register', controller.registerUser)
 
         return router
     }
