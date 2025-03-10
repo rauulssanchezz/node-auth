ac27f3bdacef1afb70ba93333bf921b4d614e096
Raúl Sánchez <132095151+rauulssanchezz@users.noreply.github.com>
2025-03-10 17:04:14 +0100

    INITIAL COMMIT node RestServer con Clean Arquitecture

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import { envs } from "./config"
+import { Server } from "./presentation/server"
+
+(() => {
+    main()
+})()
+
+async function main() {
+    new Server({
+        port: envs.PORT
+    }).start()
+}
---
+++ src/presentation/server.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+
+interface Options {
+    port?: number
+}
+
+export class Server {
+
+    public readonly app = express()
+    private readonly port: number
+
+    constructor( options: Options ) {
+        const { port = 3000 } = options
+
+        this.port = port
+    }
+
+    async start() {
+        this.app.listen(this.port, () => {
+            console.log(`Server running on port ${this.port}`)
+        })
+    }
+}
