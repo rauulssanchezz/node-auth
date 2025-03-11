1d003b3a38c6c6d9fa39b4992d19608af074dde2
Raul Sanchez <rsanchez1@certinia.com>
2025-03-11 17:23:04 +0100

    FEAT: userDto creation

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { RegisterUserDto } from "../../domain"
 
 
 
@@ -6,7 +7,10 @@ export class AuthController {
     constructor() {}
 
     registerUser = (req: Request, res: Response) => {
-        res.json('registerUser controller')
+        const [error, registerUserDto] = RegisterUserDto.create(req.body)
+        if ( error ) return res.status(400).json({ error })
+
+        res.json( registerUserDto )
     }
 
     loginUser = (req: Request, res: Response) => {
