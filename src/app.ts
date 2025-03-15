61c967c9a3f107a899997af124f061aa8d73588d
Raul Sanchez <rsanchez1@certinia.com>
2025-03-15 22:45:24 +0100

    FEAT: Mongo connect

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { envs } from "./config"
+import { MongoDatabase } from "./data/mongodb"
 import { AppRoutes } from "./presentation/routes"
 import { Server } from "./presentation/server"
 
@@ -7,6 +8,12 @@ import { Server } from "./presentation/server"
 })()
 
 async function main() {
+
+    await MongoDatabase.connect({
+        dbName: envs.MONGO_DB_NAME,
+        mongoUrl: envs.MONGO_URL,
+    })
+
     new Server({
         port: envs.PORT,
         routes: AppRoutes.routes
