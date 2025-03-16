4ac01edf74aeea520af4495ad8e6f61793596b06
Raul Sanchez <rsanchez1@certinia.com>
2025-03-16 09:43:34 +0100

    FEAT: HandleErrors

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express"
-import { AuthRepository, RegisterUserDto } from "../../domain"
+import { AuthRepository, CustomError, RegisterUserDto } from "../../domain"
 
 
 
@@ -8,13 +8,22 @@ export class AuthController {
         private readonly authRepository: AuthRepository,
     ) {}
 
+    private handleError = ( error: unknown, res: Response ) => {
+        if ( error instanceof CustomError ) {
+            return res.status(error.statusCode).json({ error: error.message })
+        }
+
+        console.log(error)
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
+
     registerUser = (req: Request, res: Response): void => {
         const [error, registerUserDto] = RegisterUserDto.create(req.body)
         if ( error ) throw res.status(400).json({ error })
 
         this.authRepository.register(registerUserDto!)
         .then( user => res.json(user) )
-        .catch( error => res.status(500).json(error) )
+        .catch( error => this.handleError( error, res ) )
     }
 
     loginUser = (req: Request, res: Response) => {
