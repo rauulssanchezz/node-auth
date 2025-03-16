f5f3e5169c2f32d778fb1f6c6194126948ba8886
Raul Sanchez <rsanchez1@certinia.com>
2025-03-16 09:19:21 +0100

    FEAT: Bcrypt implementation in auth.datasource.impl

--- src/infrastructure/datasource/auth.datasource.impl.ts
+++ src/infrastructure/datasource/auth.datasource.impl.ts
@@ -2,10 +2,16 @@ import { BcryptAdapter } from "../../config";
 import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, User } from "../../domain";
 
-
+type HashFunction = (password: string) => string
+type CompareFunction = (password: string, hashed: string) => boolean
 
 export class AuthDatasourceImpl implements AuthDatasource {
-    
+
+    constructor(
+        private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+        private readonly comparePassword: CompareFunction = BcryptAdapter.compare
+    ) {}
+
     async register(registerUserDto: RegisterUserDto): Promise<User> {
         
         const { name, email, password } = registerUserDto
@@ -18,7 +24,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
             const user = await UserModel.create({
                 name: name,
                 email: email,
-                password: BcryptAdapter.hash( password )
+                password: this.hashPassword( password )
             })
 
             await user.save()
