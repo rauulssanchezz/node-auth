a30e11966d0ce9ff5ce90c2ca5dc68ed01000a65
Raul Sanchez <rsanchez1@certinia.com>
2025-03-21 14:13:00 +0100

    UPDATE: update for login

--- src/domain/datasources/auth.datasources.ts
+++ src/domain/datasources/auth.datasources.ts
@@ -1,9 +1,10 @@
+import { LoginUserDto } from "../dtos/auth/login-user.dto";
 import { RegisterUserDto } from "../dtos/auth/register-user.dto";
 import { User } from "../entities/user.entity";
 
 export abstract class AuthDatasource {
 
-    //abstract login()
+    abstract login( loginUserDto: LoginUserDto ): Promise<User>
 
     abstract register(registerUserDto: RegisterUserDto): Promise<User>
 }
